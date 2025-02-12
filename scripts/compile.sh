bf05b77ca92c644a2da6ece0d7b76fbb082cdf12
Vincent <rabier@hotmail.fr>
2025-02-12 20:42:50 +0100

    Add build on osx

---
+++ scripts/compile.sh
@@ -0,0 +1,13 @@
+
+# Go to the build folder inside the root folder (working dir should be the script folder)
+cd ..
+mkdir build
+cd build 
+
+# Erase build files
+rm -f *.o *.sym *.map *.gb
+
+# Compile
+../bin/macos/rgbasm -o stardiewcheapley.o -I ../sources ../sources/stardiewcheapley.asm
+../bin/macos/rgblink -o stardiewcheapley.gb --map stardiewcheapley.map --sym stardiewcheapley.sym stardiewcheapley.o 
+../bin/macos/rgbfix -v -p 0xFF stardiewcheapley.gb
